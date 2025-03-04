8d5a49e9642ac42a9ea8ae4df92d8ef0d0f86d21
princeprakhar <prakhardeep173@gmail.com>
2025-03-04 15:29:51 +0530

    config the front with deplyed backend link in production

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,7 +24,7 @@ app.add_middleware(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://prakhar-ai-pdf.vercel.app/","http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
