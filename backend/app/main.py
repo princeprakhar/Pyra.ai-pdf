1845569b8dec4261ef941aa8960a30885f5ec856
princeprakhar <prakhardeep173@gmail.com>
2025-03-04 21:57:29 +0530

    updated the cor for backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,12 +24,11 @@ app.add_middleware(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://prakhar-ai-pdf.vercel.app/","http://localhost:3000"],
+    allow_origins=["https://prakhar-ai-pdf.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 app.middleware("http")(log_requests)
 
 # Include routers
